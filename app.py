1e40c4089a1438d2aaa143a4b503e1c41fe888aa
javad pournosrat <pournosrat.javad@gmail.com>
2023-01-24 21:20:55 +0330

    4фx

--- app.py
+++ app.py
@@ -7,23 +7,52 @@ list_of_standard_reber = [6, 8, 10, 12, 14, 16, 18, 20, 22, 25, 28, 32, 36, 40, 
 #sqrt = math.sqrt(f)
 
 # 3фx
+def three():
 
-a = 4*As
-b = 3*math.pi
+    a = 4*As
+    b = 3*math.pi
 
-d = math.sqrt(a/b)
+    d = math.sqrt(a/b)
 
-print(d)
+    print(d)
 
-p = []
+    p = []
 
-for i in list_of_standard_reber:
-    f = i - d
-    p.append(f)
+    for i in list_of_standard_reber:
+        f = i - d
+        p.append(f)
 
-for j in p:
-    if j >= 0 and j <=2:
-        indx = p.index(j)
-        print(indx)
+    for j in p:
+        if j >= 0 and j <=3:
+            indx3 = p.index(j)
+            #print(indx)
 
-print(list_of_standard_reber[11])
+    print(list_of_standard_reber[indx3])
+
+
+# 4фx
+
+def four():
+
+    a = As
+    b = math.pi
+
+    d = math.sqrt(a/b)
+
+    print(d)
+
+    p = []
+
+    for i in list_of_standard_reber:
+        f = i - d
+        p.append(f)
+
+    for j in p:
+        if j >= 0 and j <=3:
+            idx4 = p.index(j)
+            #print(indx)
+
+    print(list_of_standard_reber[idx4])
+
+three()
+four()
